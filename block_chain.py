c0702f4aa944e51b1cf47fb73f82f47fa0171c1b
Manolo Rajaonah <77987911+ManoloRaj@users.noreply.github.com>
2021-07-23 23:21:26 +0300

    Update block_chain.py

--- block_chain.py
+++ block_chain.py
@@ -1,1 +1,11 @@
-
+class block_chain :
+  def __init__(self):
+    self.node = null
+    self.block = null
+    
+  def register_node ():
+    print("add new node to block")
+    
+  def new_block():
+    print("add new block to chain")
+  
