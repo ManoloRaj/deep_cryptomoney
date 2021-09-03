fd5ef6abd737ecdd87e123f962053f2cf883eb1f
Manolo Rajaonah <77987911+ManoloRaj@users.noreply.github.com>
2021-09-03 12:19:39 +0300

    Add files via upload

--- block_chain.py
+++ block_chain.py
@@ -19,6 +19,8 @@ class block_chain(object) :
     self.chain.append(block)
 
     return block
+
+
   @property
   def last_block(self):
 
@@ -33,6 +35,15 @@ class block_chain(object) :
 
     self.pending_transactions.append(transaction)
     return self.last_block['index'] + 1
+
+  def hash(self, block):
+    string_object = json.dumps(block, sort_keys=True)
+    block_string = string_object.encode()
+
+    raw_hash = hashlib.sha256(block_string)
+    hex_hash = raw_hash.hexdigest()
+
+    return hex_hash
   
 if __name__ == "__main__":
     manolo_blockChain = block_chain()
