d2a66716bac6b63457022ac1b8547bc81e22decb
Manolo Rajaonah <77987911+ManoloRaj@users.noreply.github.com>
2021-09-03 12:10:45 +0300

    new block function

--- block_chain.py
+++ block_chain.py
@@ -8,6 +8,17 @@ class block_chain(object) :
     self.pending_transactions = []
     self.new_block(previous_hash="The Times of today ... sell to banks", proff=100)
 
+  def new_block(self, proff, previous_hash=None):
+    block = {
+      'index'         : len(self.chain) + 1,
+      'timestamp'     : time(),
+      'transactions'  : self.pending_transactions,
+      'proof'         : previous_hash or self.hash(self.chain[-1])
+    }
+    self.pending_transactions = []
+    self.chain.append(block)
+
+    return block
   
 if __name__ == "__main__":
-    pass
+    manolo_blockChain = block_chain()
